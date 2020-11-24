c3187945f03de253adc7d26f0471a7671f3f2260
Alexey <44061702+samarec1812@users.noreply.github.com>
2020-11-24 15:53:03 +0300

    Add files via upload

---
+++ Lab6/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(382, 237);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Paint += new System.Windows.Forms.PaintEventHandler(this.button1_Paint);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(382, 281);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Paint += new System.Windows.Forms.PaintEventHandler(this.button2_Paint);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(536, 281);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Paint += new System.Windows.Forms.PaintEventHandler(this.button3_Paint);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(536, 237);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Paint += new System.Windows.Forms.PaintEventHandler(this.button4_Paint);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 59);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(255, 255);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(293, 183);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(293, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 326);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Lab6/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        bool firstPl = true,  fl3 = true;
+        int score1 = 0, score2 = 0;
+        int[,] mas = new int[5,5];
+        public Form1()
+        {
+            InitializeComponent();
+            RefreshMas(ref mas);
+        }
+
+        private void button1_Paint(object sender, PaintEventArgs e)
+        {
+            Draw(e);
+        }
+
+        private void button2_Paint(object sender, PaintEventArgs e)
+        {
+            Draw(e);
+        }
+
+        private void button3_Paint(object sender, PaintEventArgs e)
+        {
+            Draw(e);
+        }
+
+        private void button4_Paint(object sender, PaintEventArgs e)
+        {
+            Draw(e);
+        }
+
+        
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        { 
+             e.Graphics.DrawEllipse(new Pen(Color.Blue, 3), new Rectangle(320, 20, 50, 50));
+             e.Graphics.DrawEllipse(new Pen(Color.Yellow, 3), new Rectangle(350, 40, 50, 50));
+             e.Graphics.DrawEllipse(new Pen(Color.Black, 3), new Rectangle(380, 20, 50, 50));
+             e.Graphics.DrawEllipse(new Pen(Color.Green, 3), new Rectangle(410, 40, 50, 50));
+             e.Graphics.DrawEllipse(new Pen(Color.Red, 3), new Rectangle(440, 20, 50, 50));
+
+             e.Graphics.DrawLine(new Pen(Color.Blue, 3), 320 + 50, 20 + 19, 320 + 50, 20 + 25);
+             e.Graphics.DrawLine(new Pen(Color.Black, 3), 340 + 90, 20 + 19, 340 + 90, 20 + 25);
+             e.Graphics.DrawLine(new Pen(Color.Green, 3), 360 + 79, 20 + 20, 360 + 83, 20 + 20);
+             e.Graphics.DrawLine(new Pen(Color.Yellow, 3), 340 + 79 - 41, 20 + 20, 340 + 83 - 40, 20 + 20);  
+        }
+        
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+
+                Pen p = new Pen(Color.Black, 1);
+                e.Graphics.FillRectangle(Brushes.AntiqueWhite, new Rectangle(1, 1, 255, 255));
+                for (int i = 0; i < 6; ++i)
+                {
+                    e.Graphics.DrawLine(p, 50 * i, 0, 50 * i, 254);
+                    e.Graphics.DrawLine(p, 0, 50 * i, 254, 50 * i);
+                }
+                fl3 = false;          
+        }
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+            int x = e.X / 51;
+            int y = e.Y / 51;
+            Graphics dc = pictureBox1.CreateGraphics();
+            Rectangle rec = new Rectangle(x * 50 , y * 50 , 50, 50);
+            if (firstPl)
+            {
+                var br = new LinearGradientBrush(rec, Color.FromArgb(128,200,50,100), Color.Violet, LinearGradientMode.Horizontal);
+                Image img = Image.FromFile("C:\\Users\\ПК\\Downloads\\ico.bmp");
+                var PicBr = new TextureBrush(img);//C:\Users\ПК\Downloads
+                dc.FillRectangle(br, rec);
+                dc.FillRectangle(PicBr, rec);
+               
+                mas[x, y] = 1;
+            }
+            else
+            {
+                var br = new LinearGradientBrush(rec, Color.DeepPink, Color.Orange, LinearGradientMode.Vertical);
+                Image img = Image.FromFile("C:\\Users\\ПК\\Downloads\\ico.bmp");
+                var PicBr = new TextureBrush(img);
+                dc.FillRectangle(br, rec);
+                dc.FillRectangle(PicBr, rec);
+                
+                mas[x, y] = 2;
+            }
+
+            var winner = Winner();
+            if (winner != 0)
+            {
+                if (winner == 1)
+                    ++score1;
+                else
+                    ++score2;
+                label1.Text = "Счет " + Convert.ToString(score1) + " : " + Convert.ToString(score2);
+              
+                label2.Text = "Победил " + (winner == 1 ? "первый " : "второй ") + "игрок.";
+                Refresh();
+                RefreshMas(ref mas);
+                firstPl = true;
+            }
+            else
+                if (Full(mas))
+            {
+                label2.Text = "Ничья.";
+                Refresh();
+                RefreshMas(ref mas);
+                firstPl = true;
+            }
+            else
+                firstPl = !firstPl;
+        }
+        
+        private int Winner()
+        {
+            for (int i = 0; i < 5; i++)
+                for (int j = 0; j < 2; j++)
+                    if (mas[i, j] > 0)
+                    {
+                        for (int k = j + 1; k < 5; k++)
+                        {
+                            if (mas[i, k] != mas[i, j])
+                                break;
+                            else
+                            if (k - j == 3)
+                                if (mas[i, j] == 1)
+                                    return 1;
+                                else
+                                    return 2;
+                        }
+                    }
+            for (int i = 0; i < 2; i++)
+                for (int j = 0; j < 5; j++)
+                    if (mas[i, j] > 0)
+                    {
+                        for (int k = i + 1; k < 5; k++)
+                        {
+                            if (mas[k, j] != mas[i, j])
+                                break;
+                            else
+                            if (k - i == 3)
+                                if (mas[i, j] == 1)
+                                    return 1;
+                                else
+                                    return 2;
+                        }
+                    }
+
+
+            return 0;  
+        }   
+
+        static void Draw(PaintEventArgs e)
+        {
+           
+            e.Graphics.FillRectangle(new SolidBrush(Color.LightCoral), 1, 1, 100, 50);
+            e.Graphics.FillRectangle(new SolidBrush(Color.Gold), 3, 3, 50, 50);
+            e.Graphics.FillEllipse(new SolidBrush(Color.Aquamarine), 44, 0, 90, 45);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //button1_Paint(sender, e); 
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public static void RefreshMas(ref int[,] mas)
+        {
+            for (int i = 0; i <= mas.GetUpperBound(0); i++)
+                for (int j = 0; j <= mas.GetUpperBound(1); j++)
+                    mas[i, j] = 0;
+        }
+        public static bool Full( int[,] mas)
+        {
+            for (int i = 0; i <= mas.GetUpperBound(0); i++)
+                for (int j = 0; j <= mas.GetUpperBound(1); j++)
+                    if (mas[i, j] == 0)
+                        return false;
+            return true;
+        }
+    }
+}
